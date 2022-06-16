c86c42c9d61e2cfad31b32819074ebe9df3a18a2
wellpinho <wellpinho@outlook.com>
2022-06-16 14:42:54 -0300

    ✨ NEW FEATURE: header component

---
+++ ignews/src/component/header/index.tsx
@@ -0,0 +1,18 @@
+import style from "./styles.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={style.header}>
+      <div className={style.container}>
+        <img src="/images/logo.svg" alt="ig-news" />
+
+        <nav>
+          <a href="" className={style.active}>
+            Home
+          </a>
+          <a href="">Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
